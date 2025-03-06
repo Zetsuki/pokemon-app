2adce2d58193f2aa1467eaf1646f4b8f5c69cf6a
Zetsuki <zetsukiOG@gmail.com>
2025-03-06 14:26:24 +0100

    Components logic tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  template: `<h1>Liste de Pokémons</h1><router-outlet />`,
   standalone: false,
   styles: []
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+    this.selectPokemon(POKEMONS[0]);
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur ${pokemon.name}`);
+  }
 }
