c13acd18321fca50d6646f1f4176c971c4623e3c
Zetsuki <zetsukiOG@gmail.com>
2025-03-14 20:39:29 +0100

    Reordered the app by using modules

---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailsPokemonComponent } from './details-pokemon/details-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { RouterModule, Routes } from '@angular/router';
+
+const pokemonRoutes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailsPokemonComponent},
+];
+
+@NgModule({
+  declarations: [
+    ListPokemonComponent,
+    DetailsPokemonComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(pokemonRoutes)
+  ]
+})
+export class PokemonModule { }
